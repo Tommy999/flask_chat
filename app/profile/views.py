5cc166d7ec99d70039b39da7d6e7c9c9c9d8a244
Tommy999 <karpenko_tolia4@ukr.net>
2015-11-18 18:20:44 +0200

    new conainers, user list

--- app/profile/views.py
+++ app/profile/views.py
@@ -1,13 +1,18 @@
 from . import profile
 from flask import render_template, flash, redirect, request, url_for, session
 from flask.ext.login import login_required
+from ..models import *
+
 
 @profile.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
 
     name = session['auth_user']
-
-    return render_template('profile.html', name=name)
+    workgroup_id = User.query.filter_by(username=session['auth_user']).first()
+    contact_list = User.query.filter_by(workgroup_id=workgroup_id.workgroup_id).all()
+    query_set = []
+    [query_set.append(elem.username) for elem in contact_list]
+    return render_template('profile.html', name=name, contact_list=query_set)
 
 
